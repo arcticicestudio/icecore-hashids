bd59da3434c69e11f92c41e152acc04e9fae1e88
Arctic Ice Studio <development@arcticicestudio.com>
2016-06-06 21:08:44 +0200

    Create public API test class "HashidsTest"
    Tests the "IceCore - Hashids" public API class "Hashids".

---
+++ src/test/java/com/arcticicestudio/icecore/hashids/HashidsTest.java
@@ -0,0 +1,40 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     Hashids Public API Test         +
+project   icecore-hashids                 +
+file      HashidsTest.java                +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-06-06 21:05 UTC+0200       +
+modified  2016-06-06 21:06 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Tests the "IceCore - Hashids" public API class "Hashids".
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+Hashids
+  (http://hashids.org)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+Arctic Versioning Specification (ArcVer)
+  (http://specs.arcticicestudio.com/arcver)
+*/
+package com.arcticicestudio.icecore.hashids;
+
+/**
+ * Tests the <a href="https://bitbucket.org/arcticicestudio/icecore-hashids">IceCore - Hashids</a>
+ * public API class {@link Hashids}.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @see <a href="https://bitbucket.org/arcticicestudio/icecore-hashids">IceCore - Hashids</a>
+ * @see <a href="http://hashids.org">Hashids</a>
+ * @since 0.1.0
+ */
+public class HashidsTest {}
