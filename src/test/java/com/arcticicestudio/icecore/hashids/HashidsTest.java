9f628e3197c66e81ea01f2396534102e3313bfc4
Arctic Ice Studio <development@arcticicestudio.com>
2016-06-09 13:15:45 +0200

    Implement test method "+ oneLongNumberWithCustomSeparators():void"

--- src/test/java/com/arcticicestudio/icecore/hashids/HashidsTest.java
+++ src/test/java/com/arcticicestudio/icecore/hashids/HashidsTest.java
@@ -98,6 +98,17 @@ public class HashidsTest {
   }
 
   @Test
+  public void oneLongNumberWithCustomSeparators() {
+    Hashids hashids = new Hashids("salt", 0, Hashids.DEFAULT_ALPHABET, "abcdefgABCDEFG1234567");
+    long number = 12_345L;
+    String hash = hashids.encodeToString(number);
+    long[] decoded = hashids.decodeLongNumbers(hash);
+
+    assertEquals(1, decoded.length);
+    assertEquals(number, decoded[0]);
+  }
+
+  @Test
   public void oneLongNumberWithLargeCustomMinimumHashLength() {
     Hashids hashids = new Hashids("salt", 1000);
     long number = 12_345;
