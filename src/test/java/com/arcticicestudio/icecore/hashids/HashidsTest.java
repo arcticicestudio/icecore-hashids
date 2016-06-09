7b251dde039e1d66a8c11d19663c377dcfe8cd61
Arctic Ice Studio <development@arcticicestudio.com>
2016-06-09 12:04:33 +0200

    Implement test method "+ severalLongNumbersWithCustomLowerCaseAlphabet():void"

--- src/test/java/com/arcticicestudio/icecore/hashids/HashidsTest.java
+++ src/test/java/com/arcticicestudio/icecore/hashids/HashidsTest.java
@@ -319,6 +319,18 @@ public class HashidsTest {
   }
 
   @Test
+  public void severalLongNumbersWithCustomLowerCaseAlphabet() {
+    Hashids hashids = new Hashids("salt", "abcdefghijklmnpqrstuvwxyz123456789");
+    long[] numbers = {683L, 94_108L, 123L, 5L};
+    String expected = "38wdt3wjqf65t2";
+    long[] decoded = hashids.decodeLongNumbers(hashids.encodeToString(numbers));
+
+    assertEquals(expected, hashids.encodeToString(numbers));
+    assertEquals(decoded.length, numbers.length);
+    assertArrayEquals(decoded, numbers);
+  }
+
+  @Test
   public void severalIntegerNumbers() {
     Hashids hashids = new Hashids("salt");
     int[] numbers = {683, 94_108, 123, 5};
